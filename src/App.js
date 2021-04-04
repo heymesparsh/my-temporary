07b9b75e7f577297291a4eb905c659cc5a43a38b
Sparsh Aggarwal <heymesparsh@gmail.com>
2021-04-04 15:29:03 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='loginpage'>
+      <div className='dine-overlay'>
+        <div className='automate-takeaway'>
+          <b>Automate your takeaway</b>{' '}
+        </div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pos features help your business to be fully automated with <br />{' '}
+          DineOrder online ordering system.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href='#' class='login-box' tabindex='0'>
+          View More
         </a>
-      </header>
+      </div>
+      <div className='log-section'>
+        <div className='attrib user-input-wrp'>
+          <form action=''>
+            <input type='text' className='inputText' required />
+            <span class='floating-label'>Email Address Or Phone Number</span>
+            <br />
+            <input type='password' className='inputText-1' required />
+            <span class='floating-label-1'>Password</span>
+            <br />
+            <input className='btn' type='button' value='LOG IN' />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
